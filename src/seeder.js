c48fe76369e3b949f97c646f111c26ed7eff1018
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-03-12 17:40:01 +0530

    Update seeder with reviews data!

--- src/seeder.js
+++ src/seeder.js
@@ -8,9 +8,10 @@ dotenv.config({
 });
 const { MONGO_URI } = process.env;
 
+const User = require("./models/User");
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
-const User = require("./models/User");
+const Review = require("./models/Review");
 
 connect(MONGO_URI, {
   useNewUrlParser: true,
@@ -19,6 +20,10 @@ connect(MONGO_URI, {
   useUnifiedTopology: true,
 });
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`),
+  "utf-8",
+);
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`),
   "utf-8",
@@ -27,16 +32,17 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`),
   "utf-8",
 );
-const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/users.json`),
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`),
   "utf-8",
 );
 
 const importData = async () => {
   try {
+    await User.create(users);
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
-    await User.create(users);
+    await Review.create(reviews);
     console.log("Data Imported...".green.inverse);
     process.exit();
   } catch (error) {
@@ -46,9 +52,10 @@ const importData = async () => {
 
 const destroyData = async () => {
   try {
+    await User.deleteMany();
     await Bootcamp.deleteMany();
     await Course.deleteMany();
-    await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data Destroyed...".green.inverse);
     process.exit();
   } catch (error) {
